4fefac8f43cb6b903ba4806cbcb7d2d813bc79dc
Clément <cironclement.pro@gmail.com>
2022-06-03 21:48:40 +0200

    pb2

---
+++ problem2/problem2.c
@@ -0,0 +1,25 @@
+/* By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms */
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	tab[100000];
+	int ra = 1;
+	int rb = 1;
+	int r;
+
+	int c = 0;
+	while (r <= 4000000)
+	{
+		r = ra + rb;
+		ra = rb;
+		rb = r;
+		if (r % 2 == 0)
+		{
+			c = c+r;
+		}
+	}
+	printf("%d",c);
+
+}
